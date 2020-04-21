3aa60150f968842a8b193732b54059683fd050b4
diego <cesarediego6@gmail.com>
2020-04-21 10:24:28 -0300

    Novas configuraçoes 21/04/20

--- .config/polybar/scripts/atlz.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-update=$(pacman -Qu | wc -l)
-
-if [[ "$update" > 0 ]]; then
-	echo " $update"
-else
-	echo ""
-fi
--- .config/polybar/scripts/cache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cache=$(ls /var/cache/pacman/pkg | wc -l)
-
-	if [[ $cache > 0 ]]; then
-		echo " $cache"
-	else
-		echo " Clean"
-	fi
